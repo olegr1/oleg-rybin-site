f39f77de2d084ba25d10a8c36c630e97ef83ce37
Oleg Rybin <you@example.com>
2024-06-03 21:51:24 -0400

    Implementing CSS and JS inlining, minification and other optiizations

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,7 @@
+const { minify } = require("terser");
+const CleanCSS = require("clean-css");
+const htmlmin = require("html-minifier");
+
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addFilter("formatMonthYear", function(partialDate) { 
@@ -15,9 +19,34 @@ module.exports = function(eleventyConfig) {
       return formattedDate;
   });
 
-  eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addNunjucksAsyncFilter("jsmin", async function (code, callback) {
+    try {
+      const minified = await minify(code);
+      callback(null, minified.code);
+    } catch (err) {
+      console.error("Terser error: ", err);
+      callback(null, code);
+    }
+  });
 
-  eleventyConfig.addWatchTarget("src/assets/css/");
+  eleventyConfig.addFilter("cssmin", function (code) {
+		return new CleanCSS({}).minify(code).styles;
+	});
+
+  eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
+    if (outputPath.endsWith(".html")) {
+      return htmlmin.minify(content, {
+        collapseWhitespace: true,
+        removeComments: true,  
+        useShortDoctype: true,
+      });
+    }
+
+    return content;
+  });
+
+  eleventyConfig.addPassthroughCopy("src/assets/images");
+  eleventyConfig.addPassthroughCopy({ "src/assets/favicon": "/" });
 
   return {
     dir: {
